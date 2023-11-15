d8617da3276bf385a392665ad317db6a4f3aae75
Nasa04 <nasastar0406@hotmail.com>
2023-11-14 19:43:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Mañeña";
+
+//var nombre1 = "Mañeña";
+
+//const nombre2 = "Mañeña";
+
+//console.log(nombre);
+//console.log(nombre1);
+//console.log(nombre2); 
+
+let menu_responsive = document.querySelector("checkbtn");
+
